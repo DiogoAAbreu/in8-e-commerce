cb83384b94986d15402dd77b8a2d6fb6d06498f2
Diogo Alberto <diogoalberto.dev@gmail.com>
2025-06-16 07:38:18 -0300

    refactor: changed search svg

--- frontend/src/components/products/SearchBar.jsx
+++ frontend/src/components/products/SearchBar.jsx
@@ -3,8 +3,8 @@ export default function SearchBar() {
         <div className="w-1/2 lg:w-1/3">
             <div className="relative">
                 <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                    <svg className="w-5 h-5 text-gray-400" viewBox="0 0 24 24" fill="none">
-                        <path d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                     </svg>
                 </span>
                 <input type="text" className="w-full bg-gray-100 border border-gray-200 rounded-lg py-2 pl-10 pr-4 focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Buscar produtos..." />
