6aaa6a92a683ae1888e8e6c5a12be45a36cb60af
Diogo Alberto <diogoalberto.dev@gmail.com>
2025-06-21 18:48:35 -0300

    refactor: changed all functions to default arrow function

--- frontend/src/components/common/Spinner.jsx
+++ frontend/src/components/common/Spinner.jsx
@@ -1,4 +1,4 @@
-const Spinner() => {
+const Spinner = () => {
     return (
         <div className="flex justify-center items-center py-10 h-full">
             <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-purple-600" />
